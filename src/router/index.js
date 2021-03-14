d70336b2bef336b638b52d7acec37ea74f1e5b18
hackftz <472849306@qq.com>
2021-03-14 22:05:51 +0800

    chore: add load function

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+function load(name) {
+  return () => import(`@/views/${name}`)
+}
+
 const routes = [
   {
     path: '/',
@@ -13,7 +17,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: load(/* webpackChunkName: "about" */ 'About.vue')
   }
 ]
 
